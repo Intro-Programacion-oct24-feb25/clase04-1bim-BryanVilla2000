88419a424d102ed63a7e7dbd8a56374d60f875c1
Bryan Villa <bryan.m.villa.c@gmail.com>
2024-10-30 11:51:50 -0500

    Agregamos Ejemplo10

--- Ejemplo01/src/ejemplo01/Ejemplo10.java
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -11,12 +11,13 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo10 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
+        int edad = 2024-nacimiento;
         
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
@@ -26,8 +27,10 @@ public class Ejemplo05 {
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);   
+        //System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        //System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);
+        
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante+"\n\t\t"+edad);
         
     }
 }
